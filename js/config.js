1af35b37da40966c572a1b717578ee239fe56906
Joshua Pelkington <pelkingtonj@gmail.com>
2026-02-22 16:47:32 -0500

    2/22/2026 Songs

--- js/config.js
+++ js/config.js
@@ -1,66 +1,79 @@
 const songFiles = [
-    {
-        "date": "12-13-2025",
-        "songs": [
-            "goodness-of-god",
-            "the-son-of-god-came-down",
-            "joy-to-the-world",
-            "all-sufficient-merit",
-            "hosanna-hosanna",
-            "god-of-every-grace",
-            "joy-has-dawned-upon-the-world",
-            "your-great-name",
-            "never-cease-to-praise",
-            "i-know-whom-i-have-believed",
-            "angels-we-have-heard-on-high"
-        ]
-    },
-    {
-        "date": "10-24-2025",
-        "songs": [
-            'if-christ-is-mine',
-            'nothing-but-the-blood',
-            'death-was-arrested',
-            'his-mercy-is-more',
-            'all-sufficient-merit',
-            'further-along',
-            'how-sweet-the-name-of-jesus-sounds',
-            'here-on-christ-the-rock',
-            'oh-lord-my-rock-and-my-redeemer',
-            'yet-not-i',
-            'complete-in-thee'
-        ]
-    },
-    {
-        "date": "9-19-2025",
-        "songs": [
-            'yet-not-i',
-            'his-glory-and-my-good',
-            'promises',
-            'from-the-inside-out',
-            'oh-lord-my-rock-and-my-redeemer',
-            'hosanna',
-            'holy-holy-holy',
-            'goodness-of-god',
-            'before-the-throne',
-            'never-cease-to-praise',
-            'those-who-look-to-him',
-            'all-sufficient-merit'
-        ]
-    }
+	{
+		date: "2-28-2026",
+		songs: [
+			"i-know-whom-i-have-believed",
+			"oh-christ-the-rock",
+			"all-i-have-is-christ",
+			"christ-is-mine-forevermore",
+			"christ-our-wisdom",
+			"come-to-jesus",
+			"i-serve-a-risen-savior",
+			"jesus-keep-me-near-the-cross",
+		],
+	},
+	{
+		date: "12-13-2025",
+		songs: [
+			"goodness-of-god",
+			"the-son-of-god-came-down",
+			"joy-to-the-world",
+			"all-sufficient-merit",
+			"hosanna-hosanna",
+			"god-of-every-grace",
+			"joy-has-dawned-upon-the-world",
+			"your-great-name",
+			"never-cease-to-praise",
+			"i-know-whom-i-have-believed",
+			"angels-we-have-heard-on-high",
+		],
+	},
+	{
+		date: "10-24-2025",
+		songs: [
+			"if-christ-is-mine",
+			"nothing-but-the-blood",
+			"death-was-arrested",
+			"his-mercy-is-more",
+			"all-sufficient-merit",
+			"further-along",
+			"how-sweet-the-name-of-jesus-sounds",
+			"here-on-christ-the-rock",
+			"oh-lord-my-rock-and-my-redeemer",
+			"yet-not-i",
+			"complete-in-thee",
+		],
+	},
+	{
+		date: "9-19-2025",
+		songs: [
+			"yet-not-i",
+			"his-glory-and-my-good",
+			"promises",
+			"from-the-inside-out",
+			"oh-lord-my-rock-and-my-redeemer",
+			"hosanna",
+			"holy-holy-holy",
+			"goodness-of-god",
+			"before-the-throne",
+			"never-cease-to-praise",
+			"those-who-look-to-him",
+			"all-sufficient-merit",
+		],
+	},
 ];
 
 const creditList = [
-    {
-        "name": "Jennifer Ou",
-        "role": "Organization"
-    },
-    {
-        "name": "Daniel Yen",
-        "role": "Location"
-    },
-    {
-        "name": "Joshua Pelkington",
-        "role": "Lyrics"
-    }
-]
+	{
+		name: "Jennifer Ou",
+		role: "Organization",
+	},
+	{
+		name: "Gareth & Jess Weakly",
+		role: "Location",
+	},
+	{
+		name: "Joshua Pelkington",
+		role: "Lyrics",
+	},
+];
